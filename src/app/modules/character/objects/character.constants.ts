f8b7ec8eb5eff097e5b2ad549ce2820099856b97
CamiloMR <camilo98.2254@gmail.com>
2022-11-04 15:30:31 -0500

    Character models

---
+++ src/app/modules/character/objects/character.constants.ts
@@ -0,0 +1,31 @@
+import { Info } from 'src/app/models/info.model';
+
+export interface InfoCharacter {
+  info: Info;
+  results: Character[];
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Origin;
+  location: Location;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+interface Origin {
+  name: string;
+  url: string;
+}
+
+interface Location {
+  name: string;
+  url: string;
+}
